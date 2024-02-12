72c1bd882ad78ff7de258f63ee0f92a021d11e4c
Jeremias <jeremontoya155@gmail.com>
2024-02-12 15:16:54 -0300

    Cambios subidas2

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import os
+import csv
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def create_upload_folder():
+    # Verifica si el directorio de carga de archivos existe
+    if not os.path.exists(UPLOAD_FOLDER):
+        # Si no existe, lo crea
+        os.makedirs(UPLOAD_FOLDER)
+
+# Llama a la función para crear el directorio de carga de archivos
+create_upload_folder()
 
 @app.route('/')
 def index():
@@ -27,6 +37,16 @@ def upload():
         txt_file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'archivo.txt'))
 
     return 'Archivos subidos exitosamente!'
+
+@app.route('/download/<filename>', methods=['GET'])
+def download(filename):
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    # Si el archivo existe en la carpeta de subidas, lo envía para descargar
+    if os.path.exists(filepath):
+        return send_file(filepath, as_attachment=True)
+    else:
+        return 'El archivo no existe'
 
 if __name__ == '__main__':
     app.run(debug=True)
